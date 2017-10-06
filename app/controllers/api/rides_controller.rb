56f088616c8765b6e86385d3b47a1f4e76989e40
Cezary Karas <cezary.karas@elpassion.pl>
2017-10-06 12:42:28 +0200

    Fix intent

--- app/controllers/api/rides_controller.rb
+++ app/controllers/api/rides_controller.rb
@@ -2,7 +2,7 @@ class Api::RidesController < ApplicationController
   def create
     ride = Ride.new(ride_params)
     ride.distance = DistanceReciever.new(ride_params[:address_start],
-                                ride_params[:address_end]).call
+                                         ride_params[:address_end]).call
 
     if ride.save
       render json: {}, status: :created
